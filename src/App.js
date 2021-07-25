1b08da4d3a4e37b51e7726fab4c7a5bef831c69e
Nathan Martinez <n.mtz.cas@gmail.com>
2021-07-24 20:49:34 -0500

    Added product edit

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
+import UserEditScreen from './screens/UserEditScreen'
+import ProductListScreen from './screens/ProductListScreen'
+import ProductEditScreen from './screens/ProductEditScreen'
 
 function App() {
   return (
@@ -32,6 +35,9 @@ function App() {
             <Route path="/cart/:id?" component={CartScreen} />
 
             <Route path='/admin/userlist' component={UserListScreen} />
+            <Route path='/admin/user/:id/edit' component={UserEditScreen} />
+            <Route path='/admin/productlist' component={ProductListScreen} />
+            <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
 
           </ Container>
         </main>
--- src/screens/UserListScreen.js
+++ src/screens/UserListScreen.js
@@ -4,7 +4,7 @@ import { Table, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
-import { listUser } from '../actions/userActions'
+import { listUser, deleteUser } from '../actions/userActions'
 
 function UserListScreen({ history }) {
 
@@ -15,16 +15,22 @@ function UserListScreen({ history }) {
     const userLogin = useSelector(state => state.userLogin)
     const { userInfo } = userLogin
 
+    const userDelete = useSelector(state => state.userDelete)
+    const { success: successDelete } = userDelete
+
     useEffect(() => {
-        if(userInfo && userInfo.is_Admin){
+        if(userInfo && userInfo.is_admin){
             dispatch(listUser())
         } else {
             history.push('/login')
         }
-    }, [dispatch])
+    }, [dispatch, history, successDelete, userInfo])
 
     const deleteHandler = (id) => {
-        console.log('deleting')
+        if(window.confirm('Are you sure you want to delete this user?')){
+            dispatch(userDelete(id))
+        }
+        
     }
 
     return (
@@ -51,13 +57,13 @@ function UserListScreen({ history }) {
                                         <td>{user._id}</td>
                                         <td>{user.name}</td>
                                         <td>{user.email}</td>
-                                        <td>{user.isAdmin ? (
+                                        <td>{user.is_admin ? (
                                             <i className='fas fa-check' style={{ color: 'green' }}></i>
                                         ) : (
                                             <i className='fas fa-check' style={{ color: 'red' }}></i>
                                         )}</td>
                                         <td>
-                                            <LinkContainer to={`/admin/user/${user._id}`}>
+                                            <LinkContainer to={`/admin/user/${user._id}/edit`}>
                                                 <Button variant='light' className='btn-sm'>
                                                     <i className='fas fa-edit'></i>
                                                 </Button>
